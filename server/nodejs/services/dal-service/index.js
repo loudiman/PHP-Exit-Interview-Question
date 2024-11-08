04fbc92277fc66b10325292736897219950c0d4f
Lestat <2233915@slu.edu.ph>
2024-11-09 02:14:13 +0800

    ~ refactored published surveys endpoint

--- server/nodejs/services/dal-service/index.js
+++ server/nodejs/services/dal-service/index.js
@@ -56,13 +56,20 @@ dalService.post('/user', async(req, res) => {
 dalService.get('/surveys/:username', async(req, res)=>{
     const {username} = req.params
     try{
-        const [rows] = await SurveyDAL.getAllPublishedSurvey(username)
+        const rows = await SurveyDAL.getAllPublishedSurvey(username)
+
+        console.log(rows)
         
-        rows.isCompleted = false;
+        // Ensure `rows` is an array and add `isCompleted` to each survey
+        const updatedRows = Array.isArray(rows) 
+        ? rows.map(row => ({ ...row, isComplete: false })) 
+        : [{ ...rows, isCompleted: false }];
+
+    
 
         // Wrap the updated rows in an object with the `survey` key
         const result = {
-            survey: rows
+            surveys: updatedRows
         };
 
         res.status(200).json(result)
