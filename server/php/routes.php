3ea1120407d35541ee7b2d7a877f356576b23c63
Lou <2233672@slu.edu.ph>
2024-11-08 19:51:52 +0800

    Added routes for student survey ui/ux

--- server/php/routes.php
+++ server/php/routes.php
@@ -1,4 +1,7 @@
 <?php
 
-$router->get('/login', 'session/create.php');
-$router->post('/login', 'session/store.php');
+$router->get('/', 'session/create.php');
+$router->post('/', 'session/store.php');
+
+$router->get('/student/surveys', 'surveys/index.php');
+$router->get('/student/survey', 'surveys/show.php');
